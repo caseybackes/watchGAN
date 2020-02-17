061f87abc0e1e71a2b660cf5a7dcc5de9f6af786
Casey Backes <casey@Caseys-MacBook-Pro.local>
2020-02-17 04:07:49 -0700

    building WatchScrape class

--- src/watchscrape.py
+++ src/watchscrape.py
@@ -1,83 +1,84 @@
 from bs4 import BeautifulSoup
 import os
-
-
-data_dir = '../data/webpages'
-
-def file2html(FILENAME):
-    '''given a filename, return the BS4 object for the html file'''
-    with open(FILENAME, 'rb') as f:
-        html = BeautifulSoup(f, 'html.parser')
-        f.close()
-    return html
-
-
-# html_list = []
-# all_image_paths = []
-
-# for f in os.listdir(data_dir):
-#     # fully qualified absolute path to fileobject
-#     full_f = os.path.join(data_dir, f)
-#     print('full f: ', full_f)
-
-#     # if dir then get all image names from dir
-#     if os.path.isdir(full_f):
-#         for file in os.listdir(full_f):
-#             if '.jpg' in os.path.basename(file):
-#                 all_image_paths.append(os.path.join(full_f,file))
-#     # if html file, then transform to bs4 obj
-#     else:
-#         html_list.append(
-#             file2html(full_f)
-#             )
+from os import path
+import shutil
+import numpy as np
 
 
 
-data_dir = '../data/webpages'
-# files = os.listdir(data_dir)
-referenced_images = []
-alt_text_list = []
-# 1. for each html document in the data_dir, get all references to img src paths that start with './Amazon', and get rid of urls, and keep the alt text
-for fileobject in os.listdir(data_dir):
-    if os.path.isfile(os.path.join(data_dir, fileobject)):
-        if fileobject != ".DS_Store":
-            print(f'{fileobject} is a document')
-            html = file2html(os.path.join(data_dir, fileobject))
-            imgs = html.findAll('img')
-            src_list = []
-            for img in imgs:
-                try:
-                    src = img.attrs['src']
-                    alt = img.attrs['alt']
-                    if alt == '' or alt == 'Prime':
-                        continue
-                    if src[0:13] == './Amazon.com_':
-                        referenced_images.append((src,alt))
-                except KeyError as e:
-                    continue
-
-       
-
-# for all directories in data_dir, extract the names of the referenced images only and delete all other files
-
-for fileobject in os.listdir(data_dir):
-    full_dir_path = os.path.join(data_dir, fileobject)
-    if os.path.isdir(full_dir_path):
-        for file in os.listdir(full_dir_path):
-            full_file_path = os.path.join(full_dir_path,file)
-            if file[-4::] != '.jpg':
-                print('Deleting File: ', file)
-                os.remove(full_file_path)
+class WatchScrape():
+    def __init__(self):
+        self.data_dir = '../data/webpages'
+  
+    def file2html(self, FILENAME):
+        '''given a filename, return the BS4 object for the html file'''
+        with open(FILENAME, 'rb') as f:
+            html = BeautifulSoup(f, 'html.parser')
+            f.close()
+        return html
 
 
 
+    def collect(self):
+        referenced_images = []
+        # 1. for each html document in the data_dir, 
+        # get all references to img src paths that start 
+        # with './Amazon', and get rid of urls, and keep the alt text
+        for fileobject in os.listdir(self.data_dir):
+            if os.path.isfile(os.path.join(self.data_dir, fileobject)):
+                if fileobject != ".DS_Store":
+                    print(f'Scraping Document: {fileobject}')
+                    html = file2html(os.path.join(self.data_dir, fileobject))
+                    imgs = html.findAll('img')
+                    src_list = []
+                    for img in imgs:
+                        try:
+                            src = img.attrs['src']
+                            alt = img.attrs['alt']
+                            if alt == '' or alt == 'Prime':
+                                continue
+                            if src[0:13] == './Amazon.com_':
+                                referenced_images.append((src,alt))
+                        except KeyError as e:
+                            continue
+
+        self.referenced_images = referenced_images
 
 
+        def clean(self):
+            # Transform referened_images list into array for indexing
+            referenced_images = np.array(self.referenced_images)
 
+            # 2. for all directories in data_dir, extract the names of the referenced images only and delete all other files
 
+            for fileobject in os.listdir(self.data_dir):
+                full_dir_path = os.path.join(self.data_dir, fileobject)
+                if os.path.isdir(full_dir_path):
+                    for file in os.listdir(full_dir_path):
+                        full_file_path = os.path.join(full_dir_path,file)
 
-# html = BeautifulSoup(open('/Users/casey/Documents/DataScienceProjects/watchgan/data/webpages/Amazon.com_ watch.html'), 'html.parser')
-# img100 = html.find_all('img')[100]
-# img100.attrs['src'] 
+                        # REMOVE THE NON JPG (TARGET) IMAGE FILES
+                        if file[-4::] != '.jpg':
+                            print('Deleting File: ', file)
+                            os.remove(full_file_path)
 
+                        # MOVE THE TARGET FILES TO THE HOLISTIC IMAGE DIRECTORY
+                        src = full_dir_path #"C:\\Users\\****\\Desktop\\test1\\"
+                        dst = '../data/images/'#"C:\\Users\\****\\Desktop\\test2\\"
+
+                        files = [i for i in os.listdir(src) if i in [os.path.basename(j) for j in referenced_images[:,0] ] ]
+                        for f in files:
+                            try:
+                                shutil.move(path.join(src, f), dst)
+                            except:
+                                print("already moved this file, continuing...")
+                                continue 
+            self.cleaned=True
+
+if __name__ == "__main__":
+    
+    WS = WatchScrape()
+
+    WS.collect()
+    WS.clean()
 
