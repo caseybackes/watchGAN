d1acdb42d1f81bb6784c254114bc15c77301b8e1
Casey Backes <casey@Caseys-MacBook-Pro.local>
2020-02-17 20:57:05 -0700

    changes to watchscrape.py and new images downloaded

--- src/watchscrape.py
+++ src/watchscrape.py
@@ -3,6 +3,7 @@ import os
 from os import path
 import shutil
 import numpy as np
+import pandas as pd
 
 
 
@@ -29,7 +30,7 @@ class WatchScrape():
         # with './Amazon', and get rid of urls, and keep the alt text
         for fileobject in os.listdir(self.data_dir):
             if os.path.isfile(os.path.join(self.data_dir, fileobject)):
-                if fileobject != ".DS_Store":
+                if fileobject != ".DS_Store" and '.txt' not in fileobject:
                     print(f'Scraping Document: {fileobject}')
                     html = self.file2html(os.path.join(self.data_dir, fileobject))
                     self.html.append(html)
@@ -48,7 +49,19 @@ class WatchScrape():
                                 referenced_images.append((src,alt))
                         except KeyError as e:
                             continue
+                    try:
+                        os.remove(os.path.join(self.data_dir, fileobject))
+                    except FileNotFoundError as ef:
+                        print(ef)
+                        continue
         self.referenced_images = np.array(referenced_images)
+
+    def write_refimg(self):
+        ref_file = '../data/webpages/reflist.txt'
+        with open(ref_file, 'a') as f:
+            for pair in self.referenced_images:
+                f.write(f'{os.path.basename(pair[0])}\t{pair[1]}\n')
+            f.close()
 
     def cull(self):
         ''' 
@@ -59,11 +72,11 @@ class WatchScrape():
         # Transform referened_images list into array for indexing
         referenced_images = self.referenced_images
         # 2. for all directories in data_dir, extract the names of the referenced images only and delete all other files
-        for fileobject in os.listdir(self.data_dir): # for each fileobj/dir in data_dir
-            full_dir_path = os.path.join(self.data_dir, fileobject) # fully qualified path to fileobj/dir
+        for fileobject in os.listdir(self.data_dir): # for each fileobj/dir in 'webpages/'
+            full_dir_path = os.path.join(self.data_dir, fileobject) # fully qualified path to each item in 'webpages'/
             if os.path.isdir(full_dir_path): # if fileobj/dir is dir:
                 for file in os.listdir(full_dir_path): # for each file in dir
-                    full_file_path = os.path.join(full_dir_path,file) # flly qualifed path to file
+                    full_file_path = os.path.join(full_dir_path,file) # flly qualifed path 
 
                     # REMOVE THE NON-JPG IMAGE FILES
                     if file[-4::] != '.jpg':
@@ -82,18 +95,81 @@ class WatchScrape():
                         try:
                             shutil.move(path.join(src, f), dst)
                         except:
-                            print(f"Failed to move {f}")
+                            #print(f"Failed to move {f}")
                             continue 
 
                 # DANGER ZONE!!
-                #shutil.rmtree(full_dir_path)   # effectively: os.remove(full_dir_path)
+                shutil.rmtree(full_dir_path)   # effectively: os.remove(full_dir_path) because we got everything we wanted from these files
 
         self.is_culled=True
+
+    def to_df(self):
+        reflist = open('../data/webpages/reflist.txt', 'r').readlines()
+        refs = []
+
+        for line in reflist:
+            split_line = line.split('\t')
+            if len(split_line)>2:
+                split_line_a = split_line[0]
+                split_line_b = ' '.join(split_line[1:])
+                split_line = [split_line_a,split_line_b]
+                print("Fixed split line: ", split_line)
+            a,b = split_line
+            b = b.rstrip('\n')
+            refs.append([a,b])
+        refs = np.array(refs)
+        df = pd.DataFrame(columns = refs[0], data = refs[1::]).drop_duplicates()
+        df.reset_index().drop('index',axis=1,inplace=True)
+        df.to_csv(r'../data/webpages/reflist.txt', header=None, index=None, sep='\t', mode='a') 
+        self.df = df
+
+    def reconcile_reflist(self):
+        ''' Sometimes an images gets in and upon visual inspection, it needs to be removed (ie: not an image of a watch).
+        When deleted from the directory via Finder (in MacOS), that image file will not longer be relevent in the reflist.txt
+        file. So we should remove it from the text file. 
+        '''
+        with open('../data/webpages/reflist.txt', 'r') as f:
+            reflist= f.readlines()
+            f.close()
+
+        lines_to_remove = []
+        for num,line in enumerate(reflist[1::]): # not including the header, obviously. 
+            fname = line.split('\t')[0] 
+            if fname not in os.listdir('../data/images'): 
+                print(fname, ' does not exist in data/images') 
+                reflist.remove(line)
+                #lines_to_remove.append(num)
+        with open('../data/webpages/reflist.txt', 'w') as f:
+            # f.write('filename\talt_text\n')
+            for line in reflist:
+                f.write(line)
+            f.close()
+
+        self.referenced_images= reflist
+
+
 
 if __name__ == "__main__":
     
     WS = WatchScrape()
 
     WS.collect()
+    WS.write_refimg()
+    WS.reconcile_reflist()
     WS.cull()
+    WS.to_df()
+
+    WS.df['alt_text'] = WS.df['alt_text'].apply(lambda x: x.replace('"', ''))
+    WS.df.drop_duplicates(inplace=True)
+    WS.df.reset_index(inplace=True)
+    WS.df.drop('index', axis=1, inplace=True)
+    os.remove('../data')
+    WS.df.to_csv(r'../data/webpages/reflist.txt', header='filename\talt_text\n', index=None, sep='\t', mode = 'a') 
+
+
+
+
+
+
+
 
