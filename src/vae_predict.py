a884b61bac1f8049b2a9087b0be702f456eeb516
Casey Backes <casey@Caseys-MacBook-Pro.local>
2020-02-24 13:35:54 -0700

    vae layer save

--- src/vae_predict.py
+++ src/vae_predict.py
@@ -26,12 +26,13 @@ INPUT_DIM = (128,128,3)
 
 vae = VariationalAutoencoder(
                 input_dim = INPUT_DIM
-                , encoder_conv_filters=[32,64,64, 64]
-                , encoder_conv_kernel_size=[3,3,3,3]
-                , encoder_conv_strides=[2,2,2,2]
-                , decoder_conv_t_filters=[64,64,32,3]
-                , decoder_conv_t_kernel_size=[3,3,3,3]
-                , decoder_conv_t_strides=[2,2,2,2]
+                , encoder_conv_filters=[32,64,64,64,128,128]
+                , encoder_conv_kernel_size=[3,3,3,3,3, 3]
+                , encoder_conv_strides=[2,2,2,2,2, 2]
+
+                , decoder_conv_t_filters=[64,64,32,3,3, 3]
+                , decoder_conv_t_kernel_size=[3,3,3,3, 3, 3]
+                , decoder_conv_t_strides=[2,2,2,2, 2, 2]
                 , z_dim=200
                 , use_batch_norm=True
                 , use_dropout=True)
